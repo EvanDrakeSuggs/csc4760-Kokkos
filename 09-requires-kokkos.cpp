07a6ce3f06cf43c209affe3415b2cbc667e45892
EvanDrakeSuggs <evandsuggs@gmail.com>
2024-02-09 12:00:20 -0500

    Altered 08-function-kokkos, added 09-requires-kokkos that uses requires.

--- 09-requires-kokkos.cpp
+++ 09-requires-kokkos.cpp
@@ -1,29 +1,31 @@
 #include <Kokkos_Core.hpp>
 #include <iostream>
 
+// 
+template<typename View_t> requires std::is_same_v<typename View_t::value_type, char>
+void foo(View_t& buf){
+  std::cout << buf.label() << " Char View works!" << std::endl;
+}
+
+// default version works on all Views
 template<typename View_t>
 void foo(View_t& buf){
   std::cout << buf.label() << " View works!" << std::endl;
-}
-
-// why does this function cause an error?
-void bar(Kokkos::View<double*>& buf){
-  std::cout << buf.label() << " View works!" << std::endl;
-}
+  }
 
 int main(int argc, char** argv) {
     	Kokkos::initialize( argc, argv );
     	{
     	int n = 8;
 	int result;
-    	Kokkos::View<int*> check("check", n);
+	// what happens if 
+	Kokkos::View<char*> check("check", n);
+	//Kokkos::View<int*> check("check", n);
 	// fill array
 	for(int i = 0; i<check.extent(0); i++){
 	  check(i) = i*i;
 	}
-	// Which one of these works correctly
 	foo(check);
-	//bar(check);
     	}
     	Kokkos::finalize();
     	return 0;
