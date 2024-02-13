6ee6d6bfe790507a8ff5e3b23906442542b0b406
EvanDrakeSuggs <evandsuggs@gmail.com>
2024-02-13 08:28:29 -0500

    Moved struct outside main that was causing issues with cuda.

--- 06-parallel-kokkos.cpp
+++ 06-parallel-kokkos.cpp
@@ -1,6 +1,12 @@
 #include <Kokkos_Core.hpp>
 #include <iostream>
 
+struct Foo{
+  KOKKOS_INLINE_FUNCTION void operator() (const double& i, double& lsum) const{
+    lsum+=i;
+  }
+};
+
 int main(int argc, char** argv) {
     	Kokkos::initialize( argc, argv );
     	{
@@ -9,10 +15,6 @@ int main(int argc, char** argv) {
 	  Kokkos::parallel_for("iterator", n, KOKKOS_LAMBDA(const int& i){
 	      check(i) = i*i;
 	    });
-	  struct Foo{
-	    KOKKOS_INLINE_FUNCTION void operator() (const double& i, double& lsum) const{
-	      lsum+=i;
-	    }	};
 	  Foo functor;
 	  double result = 0;
 	  Kokkos::parallel_reduce (n, functor, result);
